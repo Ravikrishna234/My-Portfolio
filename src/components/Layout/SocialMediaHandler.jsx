f54098eb61bd20a7e940bd87fa50ec2033f0b35e
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2023-06-10 00:36:06 -0500

    ADD:HeaderIcon

--- src/components/Layout/SocialMediaHandler.jsx
+++ src/components/Layout/SocialMediaHandler.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import classes from './SocialMediaHandler.module.css';
 
 function SocialMediaHandler(){
@@ -8,6 +8,9 @@ function SocialMediaHandler(){
         <div className= {classes['rootdiv']}>
             <a className={classes['social-media-icons']} href="https://www.linkedin.com/in/dontula-ravikrishna-905722165/">
             	<FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+            </a>
+            <a className={classes['social-media-icons']} href="https://github.com/Ravikrishna234">
+                <FontAwesomeIcon icon={faGithub} />
             </a>
         </div>
     </React.Fragment>
