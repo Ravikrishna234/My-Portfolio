f4623f26c8fbb9465ea02b3b437d20530307bcd2
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2023-06-17 23:16:41 -0500

    ADD:Description-buttons

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import classes from './Description.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faDownload} from '@fortawesome/free-solid-svg-icons';
+// import 
+// import {faDownload} from '@fortawesome/free-brands-svg-icons';
+import {Link} from 'react-router-dom';
 function Description(){
     return <>
         <div className={classes.description}>
@@ -8,6 +13,30 @@ function Description(){
             {/* <p>A Passionate </p> */}
             <p>A passionate engineer dedicated to crafting innovative solutions and driven by a desire for continuous growth</p>
             {/* ? */}
+            <div className={classes.descriptionButton}>
+                {/* <button  */}
+                {/* <a className={classes.downloadButton}> */}
+                <a href="" target="_blank">
+                {/* // l */}
+                    <button className={classes.downloadButton}>Ravi Krishna's Resume
+                    <FontAwesomeIcon icon={faDownload} style={{marginLeft:'5px'}}></FontAwesomeIcon>
+                    {/* <FontAwesomeIcon icon={faDownload} style={{marginLeft: '105px'}}></FontAwesomeIcon> */}
+
+                    {/* <FontAwesomeIcon icon={faDownload} style={{marginLeft: '10px'}}></FontAwesomeIcon> */}
+                        {/* <FontAwesomeIcon icon={faDownload} className></FontAwesomeIcon> */}
+                    </button>
+                    {/* // ? */}
+                    </a>
+                {/* </button> */}
+            {/* m} */}
+                <Link to="/contact">
+                    {/* <button  */}
+                    <button className={classes.contactButton}>Contact Me</button> 
+                    {/* <a className={classes.contactButton}>Contact Me</a> */}
+                    {/* </button> */}
+                </Link>
+                
+            </div>
         </div>
         </>
 }
--- src/components/Layout/SocialMediaHandler.jsx
+++ src/components/Layout/SocialMediaHandler.jsx
@@ -6,11 +6,22 @@ import classes from './SocialMediaHandler.module.css';
 function SocialMediaHandler(){
     return <React.Fragment>
         <div className= {classes['rootdiv']}>
-            <a className={classes['social-media-icons']} href="https://www.linkedin.com/in/dontula-ravikrishna-905722165/">
-            	<FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+            <a 
+                className={classes['social-media-icons']} 
+                href="https://www.linkedin.com/in/dontula-ravikrishna-905722165/">
+                {/* // title="Linkedin"> */}
+                {/* // title="Linkedin &#013; Dontula RAVIKRISHNA" */}
+                {/* // title=}`${LinkedIn}`> */}
+                {/* <span className={classes.linkedinTooltip}>Linkedin <br/> Dontula Ravi Krishna</span> */}
+                {/* <span className={classes.linkedinTooltip}>Dontula Ravi Krishna</span> */}
+            	{/* <FontAwesomeIcon icon={faLinkedin} title='a'></FontAwesomeIcon> */}
+                <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                {/* <span className={classes.linkedinTooltip}>Linkedin <br/> Dontula Ravi Krishna</span> */}
+                <span className={classes.tooltip}>Linkedin <br/> Dontula Ravi Krishna</span>
             </a>
             <a className={classes['social-media-icons']} href="https://github.com/Ravikrishna234">
-                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faGithub}> </FontAwesomeIcon>
+                <span className={classes.tooltip}>Github <br/> Ravikrishna234</span>
             </a>
         </div>
     </React.Fragment>
