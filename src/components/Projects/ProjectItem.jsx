a3268ea867c9da07020fd5cd3055d61c84ecac46
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2023-06-18 23:26:55 -0500

    ADD: Image-path

--- src/components/Projects/ProjectItem.jsx
+++ src/components/Projects/ProjectItem.jsx
@@ -6,7 +6,7 @@ function ProjectItem(props){
     return <>
         <Card>
           <div className={classes.image}>
-            {props.projectImages[0] && <img className={classes.projectImage} src={`../../assets/ProjectImages/` + `${props.projectImages[0]}`} /> }
+            {props.projectImages[0] && <img className={classes.projectImage} src={`assets/ProjectImages/` + `${props.projectImages[0]}`} /> }
           </div>
           <div className={classes.title}><h3>{props.name}</h3></div>
           <div className={classes.projectText} dangerouslySetInnerHTML={{ __html: props.description }}>
